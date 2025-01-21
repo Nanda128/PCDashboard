10065284afc65131784afdbda44c3872aa85d435
Nanda <nandakishorevinay@gmail.com>
2025-01-21 09:55:14 -0500

    Added logging categories to logger

--- logger.py
+++ logger.py
@@ -20,8 +20,21 @@ class LoggerManager:
         with open('config.json', 'r') as f:
             config = json.load(f)
             logging.config.dictConfig(config)
+        
+        logging_level = config.get("logging", "Extensive")
+        level_mapping = {
+            "Extensive": logging.DEBUG,
+            "WarningsOnly": logging.WARNING,
+            "ErrorsOnly": logging.ERROR,
+            "Mute": logging.CRITICAL + 1
+        }
+        level = level_mapping.get(logging_level, logging.DEBUG)
+        
+        for handler in logging.root.handlers:
+            handler.setLevel(level)
+        
         self.logger = logging.getLogger()
-        self.logger.log(logging.INFO, 'Logger initialized')
+        self.logger.log(logging.INFO, 'Logger initialized with level: {}'.format(logging_level))
 
     def get_logger(self):
         caller_frame = inspect.stack()[1]
