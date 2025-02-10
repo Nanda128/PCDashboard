6cdbe78ae55efcadc93f6b836b51963fe4d3eb44
Nanda <nandakishorevinay@gmail.com>
2025-02-10 18:31:02 -0500

    Refactor remote process management to use Flask routes, add error handling, and implement request logging via flashing

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, redirect, url_for
-from utils import get_remote_running_applications, terminate_remote_processes_by_name
+from flask import Flask, render_template, request, redirect, url_for, flash
+import requests
 import json
 
 app = Flask(__name__)
-
-with open('config.json') as config_file:
-    config = json.load(config_file)
-    config_file.close()
+app.secret_key = 'ubersecretkey'  # Needed for flashing messages
 
 @app.route('/')
 def index():
@@ -14,14 +11,35 @@ def index():
 
 @app.route('/processes')
 def processes():
-    processes = get_remote_running_applications()
+    try:
+        client_ip = request.headers.get('X-Real-IP')
+        response = requests.get(f'http://{client_ip}:{remote_user_port}/get_running_applications')
+        response.raise_for_status()
+        processes = response.json()
+    except requests.exceptions.RequestException as e:
+        flash(f'Error fetching processes: {e}', 'error')
+        processes = {}
+    except json.JSONDecodeError:
+        flash('Error decoding JSON response from server', 'error')
+        processes = {}
     return render_template('processes.html', processes=processes)
 
 @app.route('/terminate/<string:process_name>', methods=['POST'])
 def terminate(process_name):
-    process_data = json.dumps({'process_name': process_name})
-    terminate_remote_processes_by_name(process_data)
+    try:
+        client_ip = request.headers.get('X-Real-IP')
+        process_data = json.dumps({'process_name': process_name})
+        response = requests.post(f'http://{client_ip}:{remote_user_port}/terminate_processes', json=process_data)
+        response.raise_for_status()
+        flash(f'Successfully terminated processes with name: {process_name}', 'success')
+    except requests.exceptions.RequestException as e:
+        flash(f'Error terminating processes: {e}', 'error')
     return redirect(url_for('processes'))
+
+with open('mysite/config.json') as config_file:
+    config = json.load(config_file)
+    remote_user_port = config['RemoteUser']['Port']
+    config_file.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- remote_user_app.py
+++ remote_user_app.py
@@ -1,45 +1,38 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import psutil
 from logger import LoggerManager
 
 logger = LoggerManager().get_logger()
 
 app = Flask(__name__)
+CORS(app)
+
+@app.before_request
+def log_request_info():
+    logger.debug(f'Received {request.method} request for {request.url}')
+    logger.debug(f'Request headers: {request.headers}')
+    logger.debug(f'Request body: {request.get_data()}')
 
 @app.route('/get_running_applications', methods=['GET'])
 def get_running_applications():
-    """
-    Get a list of running applications and their process IDs.
-
-    Returns:
-        processes: JSON response containing a list of running applications and their process IDs.
-    """
     processes = {}
     for p in psutil.process_iter(['pid', 'name']):
         try:
-            logger.info('Processes found!')
-            if p.info['name'] and p.info['pid'] != 0:
-                logger.debug('Process name: {}, PID: {}'.format(p.info['name'], p.info['pid']))
-                if p.info['name'] not in processes:
-                    processes[p.info['name']] = []
-                    logger.debug('Adding process name to list: {}'.format(p.info['name']))
-                processes[p.info['name']].append(p.info['pid'])
+            name, pid = p.info['name'], p.info['pid']
+            if name and pid != 0:
+                processes.setdefault(name, []).append(pid)
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            logger.error('Failed to retrieve process information')
+            logger.error('Failed to retrieve process information')    
     logger.info('Retrieved running applications!')
     return jsonify(processes)
 
 @app.route('/terminate_processes', methods=['POST'])
 def terminate_processes():
-    """
-    Terminate processes with a given name.
-
-    Returns:
-        status: JSON response indicating the status of the operation.
-    """
     data = request.get_json()
     process_name = data.get('process_name')
     terminated_pids = set()
+    
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'] == process_name and proc.info['pid'] not in terminated_pids:
             try:
@@ -47,8 +40,9 @@ def terminate_processes():
                 proc.wait(timeout=3)
                 terminated_pids.add(proc.info['pid'])
             except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.TimeoutExpired):
-                logger.error('Failed to terminate process')
-    logger.info('Terminated processes with name: {}'.format(process_name))
+                logger.error(f'Failed to terminate process. Error:', exc_info=True)
+
+    logger.info(f'Terminated processes with name: {process_name}')
     return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
